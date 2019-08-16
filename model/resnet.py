7f281041d2f25cdba2cb53eb72c60a29d0fb045e
ChoSungMan <pccho9108@gmail.com>
2019-08-16 17:57:55 +0900

    update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -25,20 +25,14 @@ class Conv2d(nn.Module):
     
     def _get_input_norm(self, feat):
         f = torch.ones(1, self.in_ch, self.k_size, self.k_size)
-        #print(feat, feat*feat)
         input_norm = torch.sqrt(F.conv2d(feat*feat, f, stride=self.stride, padding=self.padding)+self.eps)
         return input_norm
 
     def forward(self, x):
-        print('x : ', x)
-        print('x shape: ', x.shape)
         x_norm = self._get_input_norm(x)
-        print('x_norm : ', x_norm)
-        print('x_norm shape: ', x_norm.shape)
         x = F.conv2d(x, self.kernel, stride=self.stride, padding=self.padding)
+        
         w_norm = self._get_filter_norm(self.kernel)
-        print('w_norm : ', w_norm)
-        print('w_norm shape: ', w_norm.shape) 
         return x
 
 
@@ -46,34 +40,12 @@ class DCNet(nn.Module):
     def __init__(self):
         super(DCNet, self).__init__()
         self.conv1 = Conv2d(3, 3, 3) 
-
-    '''
-    def _get_input_norm(self, feat, kszie, stride, pad):
-        eps = 1e-4
-        shape = [ksize, ksize, feat.get_shape()[3], 1]
-        f = torch.ones(shape)
-        input_norm = torch.sqrt(nn.Conv2d(feat*feat, f, stride=stride, padding=pad)+eps)
-        return input_norm
-    
-    def _get_filter_norm(self, filt):
-        eps = 1e-4
-        return torch.norm(filt.weight.data, 2, 1, True)
-    
-
-    def _get_conv_filter(self, shape):
-        filt = nn.Parameter(torch.Tensor(shape))
-        print('filter shape: ', filt.shape)
-
-
-    def _conv2d(self, feat, k_size, n_out, is_training, stride=1, pad=1, orth=False):
-        n_in = feat.shape[1]
-        print('n_in: ', n_in)
-        filt = self._get_conv_filter([n_out, n_in, k_size, k_size])
-    '''
+        self.bn1 = nn.BatchNorm2d(3)
         
     
     def forward(self, x):
         x = self.conv1(x) 
+        x = self.bn1(x)
 
         return x
 
@@ -85,6 +57,5 @@ if __name__ == '__main__':
 
     x = torch.randn([1,3,3,3])
     y = net(x)
-
 
 
