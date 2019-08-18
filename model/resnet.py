8a05df3302f3c95a9a1b7eb28955bf7e6dd3e1f9
ChoSungMan <pccho9108@gmail.com>
2019-08-18 16:16:15 +0900

    update resnet.py - ball conv

--- model/resnet.py
+++ model/resnet.py
@@ -12,6 +12,9 @@ class Conv2d(nn.Module):
         self.k_size = k_size
         self.kernel = self._get_conv_filter(out_ch, in_ch, k_size)
         self.eps = 1e-4
+
+        self.magnitude = magnitude
+        self.angular = angular
     
     def _get_conv_filter(self, out_ch, in_ch, k_size):
         kernel = nn.Parameter(torch.Tensor(out_ch, in_ch, k_size, k_size))
@@ -34,11 +37,25 @@ class Conv2d(nn.Module):
     '''
 
     def forward(self, x):
-        out = F.conv2d(x, self.kernel, stride=self.stride, padding=self.padding)
         
-        x_norm = self._get_input_norm(x) 
-        w_norm = self._get_filter_norm(self.kernel)
-        
+        if self.magnitude is None:
+            print('magnitude function: default')
+            out = F.conv2d(x, self.kernel, stride=self.stride, padding=self.padding)
+
+        elif self.magnitude is "ball":
+            print('magnitude function: ball convolution')
+            
+            x_norm = self._get_input_norm(x) 
+            w_norm = self._get_filter_norm(self.kernel)
+            
+            #self.kernel = torch.div(self.kernel, w_norm)
+            kernel_tensor = nn.utils.parameters_to_vector(self.kernel)
+            kernel_tensor = torch.reshape(kernel_tensor,self.kernel.shape)
+            kernel_tensor = kernel_tensor / w_norm
+            out = F.conv2d(x, self.kernel, stride=self.stride, padding=self.padding)
+
+            radius = nn.Parameter(torch.Tensor(1, 1, 1, out.shape[0]))
+            radius = nn.init.constant_(radius, 1.0) ** 2 + self.eps
         
         return out
 
@@ -47,12 +64,15 @@ class DCNet(nn.Module):
     def __init__(self):
         super(DCNet, self).__init__()
         self.conv1 = Conv2d(in_ch=3, out_ch=3, k_size=3) 
+        #self.conv1 = Conv2d(in_ch=3, out_ch=3, k_size=3, magnitude="ball") 
         self.bn1 = nn.BatchNorm2d(3)
         
     
     def forward(self, x):
         x = self.conv1(x) 
         x = self.bn1(x)
+        
+        print(x)
 
         return x
 
